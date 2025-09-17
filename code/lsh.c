0e8d1345fcaef82e5574c0c9be3444edb2ec3fb1
J3pp3Pe <jespeper@chalmers.se>
2025-09-17 09:38:38 +0200

    formatting

--- code/lsh.c
+++ code/lsh.c
@@ -23,7 +23,8 @@
 #include <string.h>
 #include <readline/readline.h>
 #include <readline/history.h>
-
+#include <signal.h>
+#include <sys/wait.h>
 #include <limits.h> // For PATH_MAX
 
 // The <unistd.h> header is your gateway to the OS's process management facilities.
@@ -40,11 +41,6 @@ int main(void)
 {
   for (;;)
   {
-    //getcwd
-    char buffer[PATH_MAX];
-    char* pwd = getcwd(buffer, PATH_MAX);
-    //printf("%s$ ", pwd);
-    
     char *line;
     line = readline("> ");
 
@@ -56,7 +52,7 @@ int main(void)
     }else if(strcmp(line, "exit") == 0)
     {
       free(line);
-      break; 
+      break;
     }
 
     // Remove leading and trailing whitespace from the line
@@ -70,10 +66,7 @@ int main(void)
       Command cmd;
       if (parse(line, &cmd) == 1)
       {
-        // Just prints cmd
         print_cmd(&cmd);
-        
-        //execute command
         execute_cmd(&cmd);
       }
       else
@@ -159,7 +152,6 @@ static void print_pgm(Pgm *p)
     printf("]\n");
   }
 }
-
 
 /* Strip whitespace from the start and end of a string.
  *
